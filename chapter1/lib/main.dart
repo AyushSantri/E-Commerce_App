aa926cfb57c8f3ab7c9c5cac649493a617d07899
AyushSantri <ayushsantri222@gmail.com>
2021-11-01 23:15:29 +0530

    created myapp class in main.dart

--- chapter1/lib/main.dart
+++ chapter1/lib/main.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'homepage.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: HomePage(),
-    ),
-  );
+    );
+  }
 }
