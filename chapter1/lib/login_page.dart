7f45c0aca0399cb40a68e92b930505390e9fca63
AyushSantri <ayushsantri222@gmail.com>
2021-11-02 00:18:06 +0530

    added image in login page

--- chapter1/lib/login_page.dart
+++ chapter1/lib/login_page.dart
@@ -6,13 +6,10 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          'Login Page',
-          style: TextStyle(
-              fontSize: 20, color: Colors.blue, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
+        child: Column(
+      children: [
+        Image.asset('images/login_image.png'),
+      ],
+    ));
   }
 }
