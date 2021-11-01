e2e34e1e89d7cc6e6edd7efd7eedb519d58479d8
AyushSantri <ayushsantri222@gmail.com>
2021-11-01 23:06:52 +0530

    added a container with Text and Center widget in Scaffold

--- chapter1/lib/homepage.dart
+++ chapter1/lib/homepage.dart
@@ -5,6 +5,12 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: const Text('Hello World'),
+        ),
+      ),
+    );
   }
 }
