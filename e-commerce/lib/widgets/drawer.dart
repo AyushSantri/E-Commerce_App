f040d0143791939fe333e38a65878eed41fb290b
AyushSantri <ayushsantri222@gmail.com>
2021-11-03 19:15:36 +0530

    added Drawer Header in Drawer()

--- e-commerce/lib/widgets/drawer.dart
+++ e-commerce/lib/widgets/drawer.dart
@@ -5,6 +5,19 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: ListView(
+        children: const [
+          DrawerHeader(
+            padding: EdgeInsets.zero,
+            child: UserAccountsDrawerHeader(
+              margin: EdgeInsets.zero,
+              accountName: Text('Ayush Santri'),
+              accountEmail: Text('ayushsantri222@gmail.com'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
