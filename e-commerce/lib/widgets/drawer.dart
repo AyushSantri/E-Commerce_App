0c036a2c2b38dd1a1c8effb920b6dc33936fc632
AyushSantri <ayushsantri222@gmail.com>
2021-11-03 19:20:49 +0530

    added image in drawer header

--- e-commerce/lib/widgets/drawer.dart
+++ e-commerce/lib/widgets/drawer.dart
@@ -2,18 +2,20 @@ import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({Key? key}) : super(key: key);
-
+  final imageURL =
+      "https://media-exp1.licdn.com/dms/image/C5603AQEU_Ub9jKbLLg/profile-displayphoto-shrink_800_800/0/1628015305119?e=1641427200&v=beta&t=I9RF82FZUcbXsYRpm_p4FwN-G6V-nmeFpVqqiMISILs";
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
-        children: const [
+        children: [
           DrawerHeader(
             padding: EdgeInsets.zero,
             child: UserAccountsDrawerHeader(
               margin: EdgeInsets.zero,
               accountName: Text('Ayush Santri'),
               accountEmail: Text('ayushsantri222@gmail.com'),
+              currentAccountPicture: Image.network(imageURL),
             ),
           ),
         ],
