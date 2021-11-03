322eb55c04d293c77f266e2d8043df2eb7fc5eb3
AyushSantri <ayushsantri222@gmail.com>
2021-11-03 19:27:26 +0530

    inserted ListView inside a container

--- e-commerce/lib/widgets/drawer.dart
+++ e-commerce/lib/widgets/drawer.dart
@@ -7,20 +7,23 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        children: [
-          DrawerHeader(
-            padding: EdgeInsets.zero,
-            child: UserAccountsDrawerHeader(
-              margin: EdgeInsets.zero,
-              accountName: Text('Ayush Santri'),
-              accountEmail: Text('ayushsantri222@gmail.com'),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(imageURL),
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.zero,
+                accountName: Text('Ayush Santri'),
+                accountEmail: Text('ayushsantri222@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageURL),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
