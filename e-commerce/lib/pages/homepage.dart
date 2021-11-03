e9f48fb64027fbaf4bf424f3fa831de0a999d43e
AyushSantri <ayushsantri222@gmail.com>
2021-11-03 20:03:37 +0530

    changed the style of to its original form

--- e-commerce/lib/pages/homepage.dart
+++ e-commerce/lib/pages/homepage.dart
@@ -8,13 +8,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        iconTheme: const IconThemeData(color: Colors.black),
-        title: const Text(
-          "Text App",
-          style: TextStyle(color: Colors.black),
-        ),
+        title: const Text("Text App"),
       ),
       body: Center(
         child: Container(
