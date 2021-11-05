37c02316cf72dfdcb0e5b8f8e220156e01686a64
AyushSantri <ayushsantri222@gmail.com>
2021-11-06 00:23:34 +0530

    decoded JSON into Object

--- e-commerce/lib/pages/homepage.dart
+++ e-commerce/lib/pages/homepage.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:chapter1/models/catalog.dart';
 import 'package:chapter1/widgets/drawer.dart';
 import 'package:chapter1/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,6 +14,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    LoadState();
+  }
+
+  LoadState() async {
+    var catalogJSON = await rootBundle
+        .loadString("files/catalog.json"); //this will convert JSON into string
+    var decodedJSON = jsonDecode(
+        catalogJSON); // this will convert string into Map i.e, key value pair
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
