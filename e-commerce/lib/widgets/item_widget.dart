2239e32565e88663e6658c5ddb56feaee922f64f
AyushSantri <ayushsantri222@gmail.com>
2021-11-09 13:22:39 +0530

    added velocityX in dependency

--- e-commerce/lib/widgets/item_widget.dart
+++ e-commerce/lib/widgets/item_widget.dart
@@ -10,18 +10,6 @@ class ItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        leading: Image.network(item.image),
-        title: Text(item.name),
-        subtitle: Text(item.desc),
-        trailing: Text(
-          "\$${item.price}",
-          textScaleFactor: 1.5,
-          style:
-              TextStyle(color: Colors.deepPurple, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
+    return Container();
   }
 }
