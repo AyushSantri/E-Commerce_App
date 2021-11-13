4c7bada19e05b52b427b9d5b402b2f4fd477ac69
AyushSantri <ayushsantri222@gmail.com>
2021-11-13 21:50:52 +0530

    styled appbar in cart page

--- e-commerce/lib/pages/cart_page.dart
+++ e-commerce/lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -6,7 +7,11 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: "Cart".text.make(),
+      ),
     );
   }
 }
