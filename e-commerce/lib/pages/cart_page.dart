c1c489b8d1bb701b00066ac77086de3ce64f8355
AyushSantri <ayushsantri222@gmail.com>
2021-11-15 20:16:31 +0530

    created cart total class

--- e-commerce/lib/pages/cart_page.dart
+++ e-commerce/lib/pages/cart_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -15,7 +16,30 @@ class CartPage extends StatelessWidget {
       body: Column(
         children: [
           Placeholder().p32().expand(),
-          Divider(),
+          const Divider(),
+          const _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$1000".text.xl5.make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {},
+            child: "Buy".text.make(),
+          ).w32(context)
         ],
       ),
     );
