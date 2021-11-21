c0a78a3b56f1b283bab54fc7cd64af9ce8972e00
AyushSantri <ayushsantri222@gmail.com>
2021-11-21 20:28:21 +0530

    updated dummy items

--- e-commerce/lib/pages/cart_page.dart
+++ e-commerce/lib/pages/cart_page.dart
@@ -1,3 +1,4 @@
+import 'package:chapter1/models/cart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -29,12 +30,13 @@ class _CartTotal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _cart = CartModel();
     return SizedBox(
       height: 200,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          "\$1000".text.xl5.make(),
+          "\$${_cart.totalPrice}".text.xl5.make(),
           30.widthBox,
           ElevatedButton(
             onPressed: () {
@@ -57,17 +59,18 @@ class _CartList extends StatefulWidget {
 }
 
 class _CartListState extends State<_CartList> {
+  final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: 5,
+        itemCount: _cart.items!.length,
         itemBuilder: (context, index) => ListTile(
               leading: const Icon(Icons.done),
               trailing: IconButton(
                 icon: const Icon(Icons.remove_circle_outline),
                 onPressed: () {},
               ),
-              title: "items".text.make(),
+              title: _cart.items![index].name.text.make(),
             ));
   }
 }
